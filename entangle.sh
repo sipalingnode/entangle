74cb798a3c4d8f6769e2ace510f5647b92965d21
Node Army <109174478+sipalingnode@users.noreply.github.com>
2023-10-19 01:23:23 +0700

    Create entangle.sh

---
+++ entangle.sh
@@ -0,0 +1,32 @@
+echo -e "\033[0;35m"
+echo  " ========================================================";
+echo  "        _ _ _        _ _ _ _ _       _ _ _ _ _                           ";
+echo  "      /  /\  \      |  _ _ _ _|    /    _ _ _ |                          ";
+echo  "     /  /  \  \     |  |_ _ _ _   |   /                                  ";
+echo  "    /  /_ _ \  \    |_ _ __   |   |  |                                   ";
+echo  "   /  /_ _ _ \  \    _ _ __|  |   |   \_ _ _ _                           ";
+echo  "  /_ /        \ _\  |_ _ _ _ _|    \ _ _ _ _ _|                          ";
+echo -e "\e[35m"
+echo "=========================================================="
+echo -e '\e[35mNode :\e[35m' AR Node
+echo -e '\e[35mTelegram Group :\e[35m' @autosultan_group
+echo -e '\e[35mTelegram Channel :\e[35m' @airdropasc
+echo "=========================================================="
+echo -e "\e[0m"
+
+sleep 2
+
+echo -e "\e[1m\e[32m Install Bahan \e[0m" && sleep 1
+sudo apt -q update
+sudo apt -qy install curl git jq lz4 build-essential
+sudo apt -qy upgrade
+
+sudo rm -rf /usr/local/go
+curl -Ls https://go.dev/dl/go1.21.3.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
+eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
+
+echo -e "\e[1m\e[32m Clone Repo \e[0m" && sleep 1
+git clone https://github.com/Entangle-Protocol/entangle-blockchain
+
+echo '=============== DONE ==================='
